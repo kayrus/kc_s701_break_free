28ef2442029aa7755a9c84661c9af51e5611adbb
kayrus <kay.diam@gmail.com>
2017-02-13 22:54:42 +0100

    Added possibility to toggle wifi without UI

--- inject_supersu/reboot_into_dload.sh
+++ inject_supersu/reboot_into_dload.sh
@@ -3,9 +3,12 @@
 adb push dcow /data/local/tmp/
 adb shell chmod 755 /data/local/tmp/dcow
 adb push dload_mode.ko /data/local/tmp/
+adb push run-as /data/local/tmp/
 adb shell /data/local/tmp/dcow /data/local/tmp/dload_mode.ko /system/lib/modules/pronto/pronto_wlan.ko
+adb shell /data/local/tmp/dcow /data/local/tmp/run-as /system/bin/run-as
+adb shell run-as svc wifi enable
+adb shell run-as svc wifi disable
 adb shell rm /data/local/tmp/dload_mode.ko
 adb shell rm /data/local/tmp/dcow
-echo "Now toggle WiFi and press enter"
-read tmp
+adb shell rm /data/local/tmp/run-as
 adb reboot
