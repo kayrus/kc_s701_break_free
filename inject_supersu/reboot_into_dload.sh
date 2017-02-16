144d30ede3b3fdf41c3be0d6cd7061b9a57b62d2
kayrus <kay.diam@gmail.com>
2017-02-16 21:18:58 +0100

    Fixed an order

--- inject_supersu/reboot_into_dload.sh
+++ inject_supersu/reboot_into_dload.sh
@@ -6,8 +6,8 @@ adb push dload_mode.ko /data/local/tmp/
 adb push run-as /data/local/tmp/
 adb shell /data/local/tmp/dcow /data/local/tmp/dload_mode.ko /system/lib/modules/pronto/pronto_wlan.ko
 adb shell /data/local/tmp/dcow /data/local/tmp/run-as /system/bin/run-as
-adb shell run-as svc wifi enable
 adb shell run-as svc wifi disable
+adb shell run-as svc wifi enable
 adb shell rm /data/local/tmp/dload_mode.ko
 adb shell rm /data/local/tmp/dcow
 adb shell rm /data/local/tmp/run-as
